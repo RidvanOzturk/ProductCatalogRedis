7d48a705af4b5ba59bed27152ed2fd028effe91f
RidvanOzturk <ozturkridvan1@gmail.com>
2025-04-29 09:48:15 +0300

    edited primary constructor

--- ProductCatalogRedis/Caching/RedisService.cs
+++ ProductCatalogRedis/Caching/RedisService.cs
@@ -1,13 +1,7 @@
 ﻿using StackExchange.Redis;
 
 namespace ProductCatalogRedis.Caching;
-
-public class RedisService : IRedisService
+public class RedisService(IConnectionMultiplexer multiplexer) : IRedisService
 {
-    private readonly IDatabase _db;
-    public RedisService(IConnectionMultiplexer multiplexer)
-    {
-        _db = multiplexer.GetDatabase();
-    }
-    public IDatabase Db => _db;
+    public IDatabase Db => multiplexer.GetDatabase();
 }
